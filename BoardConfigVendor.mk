55d2d87062d9eb4cf101724a49db6f77ab13ba81
wudilsr <wudilsr@gmail.com>
2025-05-14 16:02:11 +0800

    mt6895-common: Initial repository

---
+++ BoardConfigVendor.mk
@@ -0,0 +1,24 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+AB_OTA_PARTITIONS += \
+    apusys \
+    audio_dsp \
+    ccu \
+    dpm \
+    dtbo \
+    gpueb \
+    gz \
+    lk \
+    mcf_ota \
+    mcupm \
+    md1img \
+    mvpu_algo \
+    pi_img \
+    preloader_raw \
+    scp \
+    spmfw \
+    sspm \
+    tee \
+    vcp
