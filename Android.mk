a7612259ea5e9a32859472b67b87816d7bdde4e0
wudilsr <wudilsr@gmail.com>
2025-05-14 21:20:00 +0800

    mt6895-common: add firmware

--- Android.mk
+++ Android.mk
@@ -3,3 +3,23 @@
 #
 
 LOCAL_PATH := $(call my-dir)
+
+$(call add-radio-file-sha1-checked,radio/apusys.img,8ce67ae581f0b4ba03f5bafb4b9f55a4277eef5a)
+$(call add-radio-file-sha1-checked,radio/audio_dsp.img,d320dfdf3c50e853b6b5629a5b36c24643298160)
+$(call add-radio-file-sha1-checked,radio/ccu.img,9d9f3701caf674430a73fe0216272e02ebd744e9)
+$(call add-radio-file-sha1-checked,radio/dpm.img,e9acc4a5e1e4f7584ff4b49b1920c59de8e4e97b)
+$(call add-radio-file-sha1-checked,radio/dtbo.img,b5d82f48610f1a3a1cd04536aef2d356540e360b)
+$(call add-radio-file-sha1-checked,radio/gpueb.img,983cc25da264c54ae6190422f18e1a762ba27102)
+$(call add-radio-file-sha1-checked,radio/gz.img,954d73c8e115da87d370a937fc9e810919412512)
+$(call add-radio-file-sha1-checked,radio/lk.img,2e64e3c15cb80e09881db749e6cd922fe190d797)
+$(call add-radio-file-sha1-checked,radio/mcf_ota.img,b897244bdb7c84755231d956cfc61569b9a1cecb)
+$(call add-radio-file-sha1-checked,radio/mcupm.img,4c81bd03618fddcc126474f8440a18d1d3244c70)
+$(call add-radio-file-sha1-checked,radio/md1img.img,1d4cfc7369aee5b893b1beca2d078663963401c5)
+$(call add-radio-file-sha1-checked,radio/mvpu_algo.img,82a884d7bbc77f2157867d36352f31cde76eb0a9)
+$(call add-radio-file-sha1-checked,radio/pi_img.img,c98c7354cead2ccb31a3003ad0464fd07ab91c39)
+$(call add-radio-file-sha1-checked,radio/preloader_raw.img,9d81bc84bcfe5115273494d2e63e529b057e355a)
+$(call add-radio-file-sha1-checked,radio/scp.img,e7d76f8214534e0c7033e184e99f77900cfb9512)
+$(call add-radio-file-sha1-checked,radio/spmfw.img,ca5f03700048613a7ec27cfdde7b68185dd9b2d2)
+$(call add-radio-file-sha1-checked,radio/sspm.img,f8d6d2897f2196edf290b8e35b65798c4996f278)
+$(call add-radio-file-sha1-checked,radio/tee.img,ec412299fb246f3321d1de7ef8e5123fd4e133e6)
+$(call add-radio-file-sha1-checked,radio/vcp.img,d417db857b68dee9173e549b9bb4028636145c13)
